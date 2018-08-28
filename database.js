6b29500307e998cbbf8a7680ef07600df4a9a370
bbellot <baptiste.bellot@actimage.com>
2018-08-28 13:03:17 +0200

    add: database deleteOne method, add: code documentation

--- database.js
+++ database.js
@@ -48,6 +48,14 @@ function Collection (DB, name) {
         if(resp.result.nModified < 1) return null;
         return await me.db.collection(name).findOne({_id: new ObjectId(id)});
     };
+
+    //deleteById
+    this.deleteById = async function (id) {
+        let response = await me.db.collection(name).remove({_id: new ObjectId(id)});
+        if(response.result.nRemoved < 1) return null;
+        return response.result;
+    };
+
     //deleteALl
     this.deleteAll = async function (filter) {
         let response = await me.db.collection(name).remove(filter);
@@ -59,8 +67,10 @@ function Collection (DB, name) {
 function DataBase (cfg) {
     this.db = {};
     var me = this;
+    //connect to mongo or get opened connection if any
     ensureConnection(cfg, this);
 
+    //set up promise for database ready
     this.ready = new Promise((resolve, reject) => {
         me.defered = {
             resolve: resolve,
@@ -72,16 +82,19 @@ function DataBase (cfg) {
         me.gateways = new Collection(me, 'gateways');
     });
 
+    //set up collection request helper for given collection name
     this.registerCollection = name => {
         me.ready.then(() => {
             me[name] = new Collection(me, name);
         });
     };
 
+    //set ref to MongoError Class
     this.__driver = require('mongodb');
     this.errorClass = require('mongodb').MongoError;
     this.errorName = "MongoError";
 
+    //set some const for specific errors handling
     this.errorClass.DATABASE_ERR_CODES = {
         DUPLICATE_ENTRY: 11000
     };
@@ -97,7 +110,7 @@ const __db = (cfg) => new DataBase(cfg);
 const DBConnections = {};
 
 const ensureConnection = (cfg, __db) => {
-
+    //check if there isn't a connection with to this database already opened
     if(!DBConnections[cfg.url]) {
         //Create database connection
         (async function() {
@@ -108,14 +121,13 @@ const ensureConnection = (cfg, __db) => {
             let client;
 
             var options = {
-                server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
-
+                socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }
             };
 
             try {
                 // Use connect method to connect to the Server
                 client = await MongoClient.connect(url, options);
-
+                //save connection to database object and resolve database ready promise
                 __db.db = client.db(dbName);
                 __db.defered.resolve();
             } catch (err) {
@@ -127,26 +139,34 @@ const ensureConnection = (cfg, __db) => {
                 __db.client = client;
             }
 
+            //save current connection in memory
             DBConnections[cfg.url] = {
                 client: client,
                 base: {}
             };
             DBConnections[cfg.url].base[cfg.name]= __db;
+
+            //return connection
             return DBConnections[cfg.url];
         })();
 
 
 
     } else if(!DBConnections[cfg.url].base[name]){
+        //if connection already existe but for another database
+        //set up database connection and save it to the current mongo connection
         let c = DBConnections[cfg.url];
         __db.db = c.client.db(cfg.name);
+        //resolve database ready promise
         __db.defered.resolve();
         __db.client = c.client;
 
         c.base[cfg.name]= __db;
 
     } else {
+        //return alredy existing connection
         __db = DBConnections[cfg.url].base[cfg.name];
+        //resolve database ready promise
         __db.defered.resolve();
         return DBConnections[cfg.url];
     }
--- error-handling.js
+++ error-handling.js
@@ -1,4 +1,6 @@
 var Joi = require('joi');
+
+// Declare Api error class
 const ApiError = function(req) {
     Error.call(this, 'An error occured on path: '+this.path);
     if(req) {
@@ -8,9 +10,11 @@ const ApiError = function(req) {
 };
 ApiError.prototype = Object.create(Error.prototype);
 ApiError.prototype.constructor = ApiError;
+
+//method to check object validity according to given schema
 ApiError.prototype.invalidate = function (obj, schema, next) {
     var e = Joi.validate(obj, schema);
-
+    //if schema ins't valid throw a Validation error to the handler
     if(e && e.error) {
         this.e = e.error;
         this.message = this.e.name+": "+this.e.details.map(item => item.message).join(', ');
@@ -18,16 +22,19 @@ ApiError.prototype.invalidate = function (obj, schema, next) {
     }
     return  e && e.error !== null;
 };
+
+//method to generate not found error for requested object
 ApiError.prototype.missingEntity = function (next) {
     this.message = "Entity not found";
     this.e = {
         name: "NotFoundError",
         details: []
     };
+    //throw the Not Found error to the handler
     next(this);
 };
 
-
+//method to set custom error type and message
 ApiError.prototype.setUpError = function (message, type, details) {
     this.message = message;
     this.e = {
@@ -37,14 +44,16 @@ ApiError.prototype.setUpError = function (message, type, details) {
 
 };
 
+//customise toString method for console display
 ApiError.prototype.toString = function () {
     try {
-        return JSON.stringify(this.toJSON());
+        return JSON.stringify(this.toJSON(), 2);
     } catch (e) {
-        return "ApiError: Error can't be displayed";
+        return "Error: Error can't be displayed";
     }
 };
 
+//customize toJSON method to format response for the API
 ApiError.prototype.toJSON = function () {
     return {
         path: this.path,
@@ -55,37 +64,44 @@ ApiError.prototype.toJSON = function () {
     };
 };
 
+
+// Declare Auth error class
 const AuthError = function(req) {
     Error.call(this, 'An error occured on path: '+this.path);
     if(req) {
         this.path = req.path;
     }
-
-
 };
 AuthError.prototype = Object.create(Error.prototype);
 AuthError.prototype.constructor = AuthError;
+//set up prototype methode to the same as Api errors for toJSON, toString and setUpError
 AuthError.prototype.toJSON = ApiError.prototype.toJSON;
 AuthError.prototype.toString = ApiError.prototype.toString;
 AuthError.prototype.setUpError = ApiError.prototype.setUpError;
 
+//handle authentications errors
 var authHandleError = (error, req, res, next) => {
 
-
+    //check error kind and handle Authentication errors
     if(error instanceof AuthError) {
         if(currentConf.debug) {
+            //log the error on debug
             console.warn(new Date(), "AuthError: ", error);
         } else {
+            //remove the error stack if not on debug
             delete error.stack;
         }
 
+        //set default response to code Bad Request
         let code = 400;
         let e = error.toJSON();
-
+        //for Auth error, respond with 401 authentication error
         if(e.type === "AuthenticationError") {
+            //set header for supported Authentication method
             res.setHeader("WWW-Authenticate", "Bearer");
             code = 401;
         }
+        //for Access errors set 403 => forbidden
         if(e.type === "AccessError") {
             code = 403;
         }
@@ -102,21 +118,23 @@ var authHandleError = (error, req, res, next) => {
     next(error);
 };
 
-
+//handle API errors (=> validations errors)
 var apiHandleError = (error, req, res, next) => {
 
-
+    //check for Api error kind and handle them
     if(error instanceof ApiError) {
         if(currentConf.debug) {
-            console.warn(new Date(), "ApiError: ", error.toJSON());
+            //log the error on debug
+            console.warn(new Date(), "ApiError: ", error);
         } else {
+            //remove the error stack if not on debug
             delete error.stack;
         }
-        let code = 503;
+        //default error code to Bad request
+        let code = 400;
         let e = error.toJSON();
-        if(e.type === 'ValidationError') {
-            code = 400;
-        }
+
+        //if error is of "not found" type change code to 404 > Not found
         if(e.type === 'NotFoundError') {
             code = 404;
         }
@@ -132,21 +150,29 @@ var apiHandleError = (error, req, res, next) => {
     next(error);
 };
 
+//handle database errors
 var databaseHandleError = (error, req, res, next) => {
-
-
+    //check error type and handle database ones
     if(error instanceof DatabaseError) {
         if(currentConf.debug) {
+            //log the error on debug
             console.warn(new Date(), "DatabaseError: ", error);
         } else {
+            //remove the error stack if not on debug
             delete error.stack;
         }
+        //set default error code and message to database error message and 400 => Bad request error code
         let code = 400;
         let message = error.message;
-        if(error.name === DatabaseError.ERRORS_NAME && error.code === DatabaseError.DATABASE_ERR_CODES.DUPLICATE_ENTRY) {
+
+        //check for 'duplicates' kind of errors
+        if(error.name === DatabaseError.ERRORS_NAME && (error.code === DatabaseError.DATABASE_ERR_CODES.DUPLICATE_ENTRY || error.message.match(new RegExp('E'+DatabaseError.DATABASE_ERR_CODES.DUPLICATE_ENTRY, "ig")))) {
+            //set custom message and code - 409 => Conflict error
             code = 409;
             message = "Entity already exist, target entity cannot be saved because another entity already contain unique fields with the same value.";
         }
+
+        //throw database error
         return res.status(code).json({
             error: "DatabaseError",
             message: message,
@@ -157,14 +183,17 @@ var databaseHandleError = (error, req, res, next) => {
     next(error);
 };
 
-
+//get any undefined error, execution errors are catched here
 var anyHandleError = (error, req, res, next) => {
 
     if(currentConf.debug) {
+        //log the error on debug
         console.warn(new Date(), "Error: ", error);
     } else {
+        //remove the error stack if not on debug
         delete error.stack;
     }
+    //throw a 500 error
     return res.status(500).json({
         error: "UnknownError",
         message: error.message,
@@ -174,10 +203,12 @@ var anyHandleError = (error, req, res, next) => {
 
 };
 
+//get unhandled errors which are happening on unhandled path
 var defaultHandleError = (req, res) => {
     if(currentConf.debug) {
         console.warn(new Date(), "NotFoundError: ", req.path);
     }
+    //throw a 404 error for path
     return res.status(404).json({
         error: "Path Not Found",
         message: "path "+req.path+" is not found",
@@ -196,10 +227,12 @@ module.exports = {
     setErrorHandler: (app) => {
         let db = require('./database')(app.get('cfg').database);
         currentConf = app.get('cfg');
-        Errors.DatabaseError = db.errorClass;
 
+        //set up database error class for database errors handling
+        Errors.DatabaseError = db.errorClass;
         DatabaseError = Errors.DatabaseError;
 
+        //set various error handlers for the app
         app.use(authHandleError);
         app.use(apiHandleError);
         app.use(databaseHandleError);
